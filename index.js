0ad69993976ab4ad6ea2d617b7d41852e4220395
Benjamin Monk <Bmonk1996@yahoo.com>
2023-02-10 11:13:54 -0600

    finsihed code and error handling

--- index.js
+++ index.js
@@ -15,10 +15,27 @@ const mostFrequentWord = (text) => {
 
 const findPassword = async () => {
   // Your code goes here
-  const poemOne = await readFile("poems/starting-poem.txt", "utf-8" )
-  console.log(poemOne)
-  mostFrequentWord(poemOne)
-  let poemTwoName = mostFrequentWord(poemOne)
+  try {
+    const poemOne = await readFile("poems/starting-poem.txt", "utf-8");
+  } catch (err) {
+    console.log("unable to load poemOne");
+    return;
+  }
+  let poemTwoName = mostFrequentWord(poemOne);
+  try {
+    const poemTwo = await readFile(`poems/${poemTwoName}.txt`, "utf-8");
+  } catch (err) {
+    console.log("unable to load poemTwo");
+    return;
+  }
+  let poemThreeName = mostFrequentWord(poemTwo);
+  try {
+    const poemThree = await readFile(`poems/${poemThreeName}.txt`, "utf-8");
+  } catch (err) {
+    console.log("unable to load poemThree");
+    return;
+  }
+  console.log(mostFrequentWord(poemThree));
   
 
 }
